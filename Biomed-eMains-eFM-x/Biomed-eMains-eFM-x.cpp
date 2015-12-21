5b57abc804202dc63411e7b49c62a2d280159cb6
lenazh <zhivun@gmail.com>
2015-12-21 15:32:55 -0800

    Imported the code

--- Biomed-eMains-eFM-x/Biomed-eMains-eFM-x.cpp
+++ Biomed-eMains-eFM-x/Biomed-eMains-eFM-x.cpp
@@ -1,6 +1,257 @@
 // This is the main DLL file.
 
 #include "stdafx.h"
-
 #include "Biomed-eMains-eFM-x.h"
+#include <tchar.h>
 
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Threading;
+using namespace System::Diagnostics;
+using namespace Biomed_eMains_eFMx;
+
+/* Non-managed callback for the DLL. Called every time new data arrives. */
+void __cdecl Biomed_eMains_eFMx::SensorCallbackFunction(double *values, DWORD datalength, BYTE packetCounter) {
+	static __int64 systemTicks = 0;
+
+	systemTicks = Stopwatch::GetTimestamp();
+	DateTime time = DateTime::Now;
+
+	if (datalength)
+	{
+		double systemSeconds = 1.0 * systemTicks / Stopwatch::Frequency;
+		array<double>^ dataX = gcnew array<double>(datalength / 3);
+		array<double>^ dataY = gcnew array<double>(datalength / 3);
+		array<double>^ dataZ = gcnew array<double>(datalength / 3);
+		Biomed_eMains_eFMx::parseData(values, datalength, dataX, dataY, dataZ);
+		eMains::InvokeNewDataHandler(dataX, dataY, dataZ, systemSeconds, time, datalength);
+	}
+}
+
+/**
+ * Invokes a data handler event if one is register. The sole purpose of this function is
+ * to let the non-managed callback to call a managed event handler.
+ */
+void eMains::InvokeNewDataHandler(array<double>^ dataX, array<double>^ dataY,
+	array<double>^ dataZ, double microsecondsSinceLastData, DateTime ^ time, int samples)
+{
+	NewDataHandler(dataX, dataY, dataZ, microsecondsSinceLastData, time, samples);
+}
+
+/**
+	Splits the raw data in the buffer on three channels and puts them into the
+	corresponding managed arrays. The data is converted to uT according to
+	the sensor calibration.
+*/
+static void Biomed_eMains_eFMx::parseData(double *buf, DWORD dataCount, array<double>^ dataX,
+	array<double>^ dataY, array<double>^ dataZ)
+{
+	if (eMains::convertToMicroTesla) {
+		for (DWORD i = 0; i < dataCount / 3; i++)
+		{
+			dataX[i] = buf[i * 3] * eMains::slopeX + eMains::offsetX;
+			dataY[i] = buf[i * 3 + 1] * eMains::slopeY + eMains::offsetY;
+			dataZ[i] = buf[i * 3 + 2] * eMains::slopeZ + eMains::offsetZ;
+		}
+	}
+	else
+	{
+		for (DWORD i = 0; i < dataCount / 3; i++)
+		{
+			dataX[i] = buf[i * 3];
+			dataY[i] = buf[i * 3 + 1];
+			dataZ[i] = buf[i * 3 + 2];
+		}
+	}
+
+}
+
+/* eMains sensor object constructor. */
+eMains::eMains(int serial)
+{
+	double values[3];
+	this->serial = serial;
+
+	_ReadSlopes(serial, values);
+	slopeX = values[0];
+	slopeY = values[1];
+	slopeZ = values[2];
+
+	_ReadOffsets(serial, values);
+	offsetX = values[0];
+	offsetY = values[1];
+	offsetZ = values[2];
+}
+
+/* Locates and loads eFM-x API.dll */
+int eMains::LoadDLL()
+{
+	eMainsDLL = LoadLibrary(_T("eFM-x API.dll"));
+	if (!eMainsDLL) {
+		return -1;
+	}
+
+	_GetNumberOfDevices =
+		(GET_NUMBER_OF_DEVICES)GetProcAddress(eMainsDLL, "GetNumberOfDevices");
+	_GetRevision = (GET_REVISION)GetProcAddress(eMainsDLL, "GetRevision");
+	_GetAvailableSerialNumbers =
+		(GET_AVAILABLE_SERIAL_NUMBERS)GetProcAddress(eMainsDLL, "GetAviableSerialNumbers");
+	_IsDeviceConnected = (IS_DEVICE_CONNECTED)GetProcAddress(eMainsDLL, "IsDeviceConnected");
+	_ReadKennung = (READ_KENNUNG)GetProcAddress(eMainsDLL, "ReadKennung");
+	_DAQInitialize = (DAQ_INITIALIZE)GetProcAddress(eMainsDLL, "DAQInitialize");
+	_DAQStart = (DAQ_START)GetProcAddress(eMainsDLL, "DAQStart");
+	_DAQStop = (DAQ_STOP)GetProcAddress(eMainsDLL, "DAQStop");
+	_DAQReadData = (DAQ_READDATA)GetProcAddress(eMainsDLL, "DAQReadData");
+	_ShowErrorInformation = (SHOW_ERROR_INFORMATION)GetProcAddress(eMainsDLL, "ShowErrorInformation");
+	_ReadSlopes = (READ_SLOPES)GetProcAddress(eMainsDLL, "ReadSlopes");
+	_ReadOffsets = (READ_OFFSETS)GetProcAddress(eMainsDLL, "ReadOffsets");
+	_SetUserCalc = (SET_USER_CALC)GetProcAddress(eMainsDLL, "SetUserCalc");
+
+	if (!(_GetNumberOfDevices && _GetRevision && _GetAvailableSerialNumbers && _IsDeviceConnected &&
+		_ReadKennung && _DAQInitialize && _DAQStart && _DAQStop && _DAQReadData &&
+		_ShowErrorInformation && _ReadSlopes && _ReadOffsets && _SetUserCalc))
+	{
+		return -1;
+	}
+
+#ifdef USE_CALLBACK
+	_SetCallback = (SET_CALLBACK)GetProcAddress(eMainsDLL, "SetCallBack");
+	if (!_SetCallback)
+	{
+		return -1;
+	}
+#endif // USE_CALLBACK
+
+	return 0;
+}
+
+/**
+* Returns an array of available magnetometer serials.
+* Returns an empty array if none are available.
+*/
+List<int>^ eMains::GetAvailableSerials()
+{
+	DWORD numberOfDevices = 0;
+	array<Int32>^ serials = gcnew array<Int32>(0);
+	if (_GetNumberOfDevices(&numberOfDevices))
+	{
+		return gcnew List<int>();
+	}
+
+	DWORD *buf = (DWORD *)malloc(numberOfDevices * sizeof(DWORD));
+	if (!_GetAvailableSerialNumbers(buf, numberOfDevices) && (numberOfDevices != 0)) {
+		serials = gcnew array<Int32>(numberOfDevices);
+		Runtime::InteropServices::Marshal::Copy(IntPtr((void *)buf), serials, 0, numberOfDevices);
+	}
+
+	free(buf);
+	return gcnew List<int>(serials);
+}
+
+/**
+* Initializes the DAQ SamplingRate and other things
+*/
+int eMains::DAQInitialize(double samplingRate, Range measurementRange, int chop, int clamp)
+{
+	samplingRate *= 3;
+	return _DAQInitialize(this->serial, &samplingRate, measurementRange, chop, clamp);
+}
+
+
+/**
+ * Starts the data acquisition.
+ * If polling method is used, dreates a new thread that will call processingFunction
+ * each time non-zero number of samples arrives.
+ * If callback method is used, registers a callback with the DLL that calls
+ * processingFunction each time the data arrives.
+ */
+
+int eMains::DAQStart(bool convertToMicroTesla)
+{
+	if (this->isReading)
+		return 0;
+
+	this->convertToMicroTesla = convertToMicroTesla;
+
+#ifdef USE_CALLBACK
+	_SetCallback(this->serial, &SensorCallbackFunction, 0);
+#endif // USE_CALLBACK
+
+	int res = _DAQStart();
+	if (res)
+	{
+		return res;
+	}
+
+	//res = _SetUserCalc(this->serial, 0);
+	//if (res)
+	//{
+	//	return res;
+	//}
+
+	this->isReading = true;
+
+#ifndef USE_CALLBACK
+	Thread^ bgPollingFunction = gcnew Thread(gcnew ThreadStart(this, &eMains::SensorPollingFunction));
+	bgPollingFunction->Start();
+#endif // USE_CALLBACK
+
+	return res;
+}
+
+
+/**
+* Stops the data acquisition.
+*/
+int eMains::DAQStop()
+{
+	if (!this->isReading)
+		return 0;
+
+	this->isReading = false;
+	return _DAQStop();
+}
+
+/* Converts the error code to a string. */
+void eMains::ShowErrorInformation(int error)
+{
+	_ShowErrorInformation(error, 0);
+}
+
+
+#define DATA_BUFFER_LENGTH 1000
+#define POLLING_TIMEOUT_MS 0
+/**
+* Polls the data from the sensor (backup solution if callbacks are still not ready when
+* the delivery is scheduled). Timestamps the data, splits it in three channels and calls
+* dataProcessingFunction when the data has arrived.
+*/
+void eMains::SensorPollingFunction() {
+	DWORD dataCount = 0;
+	__int64 systemTicks = 0;
+
+	double *buf = (double*)malloc(DATA_BUFFER_LENGTH * sizeof(double));
+	while (this->isReading) {
+		int error = _DAQReadData(this->serial, buf, DATA_BUFFER_LENGTH, &dataCount, POLLING_TIMEOUT_MS);
+		systemTicks = Stopwatch::GetTimestamp();
+		DateTime time = DateTime::Now;
+
+		if (error)
+		{
+			DAQStop();
+		}
+		else
+		{
+			if (dataCount)
+			{
+				double systemSeconds = 1.0 * systemTicks / Stopwatch::Frequency;
+				array<double>^ dataX = gcnew array<double>(dataCount / 3);
+				array<double>^ dataY = gcnew array<double>(dataCount / 3);
+				array<double>^ dataZ = gcnew array<double>(dataCount / 3);
+				parseData(buf, dataCount, dataX, dataY, dataZ);
+				NewDataHandler(dataX, dataY, dataZ, systemSeconds, time, dataCount);
+			}
+		}
+	}
+	free(buf);
+}
--- Biomed-eMains-eFM-x/Biomed-eMains-eFM-x.h
+++ Biomed-eMains-eFM-x/Biomed-eMains-eFM-x.h
@@ -1,13 +1,96 @@
 // Biomed-eMains-eFM-x.h
 
 #pragma once
+#include <windows.h>
+#define USE_CALLBACK
 
 using namespace System;
+using namespace System::Collections::Generic;
+using namespace System;
 
-namespace BiomedeMainseFMx {
-
-	public ref class Class1
+namespace Biomed_eMains_eFMx {
+	/* Selectable DAC range constants. */
+	enum Range
 	{
-		// TODO: Add your methods for this class here.
+		NEG_10_TO_PLUS_10V = 0,
+		ZERO_TO_PLUS_10V = 1,
+		NEG_5_TO_PLUS_5V = 2,
+		ZERO_TO_PLUS_5V = 3
+	};
+
+	/* Native DLL functions imported from eFM-x API.dll. */
+	typedef DWORD(__cdecl* GET_NUMBER_OF_DEVICES)(DWORD* __out num);
+	typedef DWORD(__cdecl* GET_REVISION)(DWORD __in serial, BYTE* __out rev);
+	typedef DWORD(__cdecl* GET_AVAILABLE_SERIAL_NUMBERS)(DWORD* __out buf, __in DWORD len);
+	typedef DWORD(__cdecl* IS_DEVICE_CONNECTED)(DWORD __in serial, bool* __out rev);
+	typedef DWORD(__cdecl* READ_KENNUNG)(DWORD __in serial, BYTE* __out data);
+	typedef DWORD(__cdecl* DAQ_INITIALIZE)(DWORD __in serial, double* __in SamplingRate,
+		DWORD __in MeasurementRange, DWORD __in chop, DWORD __in clamp);
+	typedef DWORD(__cdecl* DAQ_START)(void);
+	typedef DWORD(__cdecl* DAQ_STOP)(void);
+	typedef DWORD(__cdecl* DAQ_READDATA)(DWORD __in serial, double* __out buf,
+		DWORD __in DataBufferLength, DWORD* __out OutputLength, DWORD __in TimeOut);
+	typedef DWORD(__cdecl* SHOW_ERROR_INFORMATION)(DWORD __in errorCode, bool __in extended);
+	typedef DWORD(__cdecl* SET_CALLBACK)(DWORD __in serial, void __in *callback, DWORD __in DataLength);
+	typedef DWORD(__cdecl* SET_USER_CALC)(DWORD __in serial, unsigned char __in on_off);
+	typedef DWORD(__cdecl* READ_SLOPES)(DWORD __in serial, double __out *values);
+	typedef DWORD(__cdecl* READ_OFFSETS)(DWORD __in serial, double __out *values);
+
+
+	/* Functions receiving callbacks and wrapping the data into managed arrays. */
+	void __cdecl SensorCallbackFunction(double *values, DWORD datalength, BYTE packetCounter);
+	static void parseData(double *buf, DWORD dataCount, array<double>^ dataX, array<double>^ dataY, array<double>^ dataZ);
+
+	public ref class eMains
+	{
+
+	public:
+		delegate void DataProcessingFunc(array<double>^ dataX, array<double>^ dataY,
+			array<double>^ dataZ, double systemSeconds, DateTime^ time, int samples);
+		eMains(int serial);
+		static int LoadDLL();
+		static List<int>^ GetAvailableSerials();
+		int DAQInitialize(double SamplingRate, Range MeasurementRange, int chop, int clamp);
+		int DAQStart(bool convertToMicroTesla);
+		int DAQStop();
+		static void ShowErrorInformation(int error);
+
+
+		static void InvokeNewDataHandler(array<double>^ dataX, array<double>^ dataY,
+			array<double>^ dataZ, double microsecondsSinceLastData, DateTime^ time, int samples);
+
+		// These functions and fields will be private and instance in future releases
+		// in order to support multiple sensors per program.
+		// The reason why they are public and static now is that the DLL callback
+		// needs to access them and it was faster to code this way.
+		static event DataProcessingFunc^ NewDataHandler;
+		static double offsetX = 0.0;
+		static double slopeX = 0.0;
+		static double offsetY = 0.0;
+		static double slopeY = 0.0;
+		static double offsetZ = 0.0;
+		static double slopeZ = 0.0;
+		static bool convertToMicroTesla = false;
+
+	private:
+		void SensorPollingFunction();
+		bool isReading = false;
+		DWORD serial;
+
+		static HINSTANCE eMainsDLL = NULL;
+		static GET_NUMBER_OF_DEVICES _GetNumberOfDevices = NULL;
+		static GET_REVISION _GetRevision = NULL;
+		static GET_AVAILABLE_SERIAL_NUMBERS _GetAvailableSerialNumbers = NULL;
+		static IS_DEVICE_CONNECTED _IsDeviceConnected = NULL;
+		static READ_KENNUNG _ReadKennung = NULL;
+		static DAQ_INITIALIZE _DAQInitialize = NULL;
+		static DAQ_START _DAQStart = NULL;
+		static DAQ_STOP _DAQStop = NULL;
+		static DAQ_READDATA _DAQReadData = NULL;
+		static SHOW_ERROR_INFORMATION _ShowErrorInformation = NULL;
+		static SET_CALLBACK _SetCallback = NULL;
+		static SET_USER_CALC _SetUserCalc = NULL;
+		static READ_SLOPES _ReadSlopes = NULL;
+		static READ_OFFSETS _ReadOffsets = NULL;
 	};
 }
